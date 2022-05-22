972d4225b2ebae74c3cee688a6b55279d844d86d
sdshuvo98 <hellosdshuvo@gamil.com>
2022-05-22 15:27:04 +0600

    business summary

--- src/pages/Home.js/BusinessSummary.js
+++ src/pages/Home.js/BusinessSummary.js
@@ -1,75 +1,90 @@
 import React from 'react';
 import CountUp from 'react-countup';
 import VisibilitySensor from 'react-visibility-sensor';
+import { HiOutlineUserGroup } from 'react-icons/hi';
+import { MdOutlineRateReview } from 'react-icons/md';
+import { BsTools, BsCashCoin } from 'react-icons/bs';
 
 const BusinessSummary = () => {
     return (
-        <div className='animate-pulse'>
+        <section className='animate-pulse my-20 text-center'>
             <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
                 <div className="grid grid-cols-2 gap-8 md:grid-cols-4">
-                    <div className="md:border-r cursor-pointer hover:text-cyan-500">
+                    <div className="md:border-r border-primary cursor-pointer">
+                        <h1 className='text-5xl lg:text-6xl flex justify-center mb-5'>
+                            <HiOutlineUserGroup />
+                        </h1>
                         <h6 className="text-4xl font-bold lg:text-5xl xl:text-6xl">
                             <VisibilitySensor partialVisibility offset={{ bottom: 200 }}>
                                 {({ isVisible }) => (
                                     <div style={{ height: 60 }}>
-                                        {isVisible ? <CountUp end={144} /> : ''}
-                                        M
+                                        {isVisible ? <CountUp end={10} /> : ''}
+                                        K+
                                     </div>
                                 )}
                             </VisibilitySensor>
                         </h6>
                         <p className="text-sm font-medium tracking-widest  uppercase lg:text-base ">
-                            Sells
+                            Customers
                         </p>
                     </div>
-                    <div className="md:border-r cursor-pointer hover:text-cyan-500">
+                    <div className="md:border-r border-primary cursor-pointer">
+                        <h1 className='text-5xl lg:text-6xl flex justify-center mb-5'>
+                            <BsCashCoin />
+                        </h1>
                         <h6 className="text-4xl font-bold lg:text-5xl xl:text-6xl">
                             <VisibilitySensor partialVisibility offset={{ bottom: 200 }}>
                                 {({ isVisible }) => (
                                     <div style={{ height: 60 }}>
-                                        {isVisible ? <CountUp end={12} /> : ''}
-                                        K
+                                        {isVisible ? <CountUp end={150} /> : ''}
+                                        M+
                                     </div>
                                 )}
                             </VisibilitySensor>
                         </h6>
                         <p className="text-sm font-medium tracking-widest  uppercase lg:text-base">
-                            Users
+                            Annual revenue
                         </p>
                     </div>
-                    <div className="md:border-r cursor-pointer hover:text-cyan-500">
+                    <div className="md:border-r border-primary cursor-pointer">
+                        <h1 className='text-5xl lg:text-6xl flex justify-center mb-5'>
+                            <MdOutlineRateReview />
+                        </h1>
                         <h6 className="text-4xl font-bold lg:text-5xl xl:text-6xl">
                             <VisibilitySensor partialVisibility offset={{ bottom: 200 }}>
                                 {({ isVisible }) => (
                                     <div style={{ height: 60 }}>
-                                        {isVisible ? <CountUp end={48} /> : ''}
-                                        K
+                                        {isVisible ? <CountUp end={22} /> : ''}
+                                        K+
                                     </div>
                                 )}
                             </VisibilitySensor>
                         </h6>
                         <p className="text-sm font-medium tracking-widest  uppercase lg:text-base">
-                            Stocks
+                            Reviews
                         </p>
                     </div>
-                    <div className=' cursor-pointer hover:text-cyan-500'>
+                    <div className=' cursor-pointer'>
+                        <h1 className='text-5xl lg:text-6xl flex justify-center mb-5'>
+                            <BsTools />
+                        </h1>
                         <h6 className="text-4xl font-bold lg:text-5xl xl:text-6xl">
                             <VisibilitySensor partialVisibility offset={{ bottom: 200 }}>
                                 {({ isVisible }) => (
                                     <div style={{ height: 60 }}>
-                                        {isVisible ? <CountUp end={34} /> : ''}
-                                        K
+                                        {isVisible ? <CountUp end={200} /> : ''}
+                                        +
                                     </div>
                                 )}
                             </VisibilitySensor>
                         </h6>
                         <p className="text-sm font-medium tracking-widest  uppercase lg:text-base">
-                            This year
+                            Car Parts
                         </p>
                     </div>
                 </div>
             </div>
-        </div>
+        </section>
     );
 };
 
